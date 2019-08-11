484dbfeff3caba9b59f9c65a9fc7bc66ebf10533
Ahmad Hamed Rangeen <rangeen.ahmad@gmail.com>
2019-08-11 15:15:41 -0700

    First React project, building content with JSX

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//Import the react and ReactDOM libraries
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Create a react component
+const App = () => {
+  const buttonText = "Click Me!";
+  const labelText = "Enter name: ";
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        {labelText}
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {buttonText}
+      </button>
+    </div>
+  );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//Take the react component and show it on the screen
+ReactDOM.render(<App />, document.querySelector("#root"));
